453fea4df1108f607088a3bdb5cbc827c2b24b51
riza55358@gmail.com <106018548+Dwkyyrz@users.noreply.github.com>
2022-11-25 22:34:41 +0700

    tes2

---
+++ koneksi.php
@@ -0,0 +1,14 @@
+<?php 
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "frozenfood";
+
+    $koneksi = mysqli_connect( $servername, $username, $password, $database);
+
+    if(!$koneksi)
+    {
+        die("Error, Please Try Again:" . mysqli_connect_error());
+    }
+    
+?>
